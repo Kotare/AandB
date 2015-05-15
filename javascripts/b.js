44fef80296fcf7465fb41af36c644bb256bae4c4
peterjacobson <peterjacobson.nz@gmail.com>
2015-05-16 02:11:24 +1200

    add trial behaviour object literal & #react to use behaviours...

--- javascripts/b.js
+++ javascripts/b.js
@@ -1,6 +1,22 @@
 function B() {
+	this.id = 1 // increment on creation?
+	this.class = 'b'
 	this.size = {x: 30, y: 30}; // superclass
-	this.coords = {x: 30, y: 30}; // (centre?) superclass
+	this.coords = {x: 30, y: 30}; // (centre!) superclass
+	this.behaviours = { // toggle?
+		default: [ function() {this.moveAbout()}, function() {this.poop()} ],
+		this.class: {
+			0: [ function() {this.procreate()} ], // continue moving?
+			5: [ function() {this.wiggle()} ]
+		},
+		'a': {
+			0: [ function() {this.stopMovingAbout()}, function() {this.shake()} ] // handle toggling behaviours like this?
+			5: [ function() {this.moveAbout()}, function() {this.poop()} ]
+		},
+		'poop': {
+			0: [ function() {this.diarreah} ] // turn off after a specified time?
+		}
+	}
 }
 
 B.prototype.time = function(world) { // superclass
@@ -16,6 +32,16 @@ B.prototype.check = function(entities) { // superclass
 										});
 		this.react(this.class, proximity) //<< make callback of proximity
 	}
+}
+
+B.prototype.react = function(class, proximity) {
+	for (var prox = proximity; prox <= 100; prox++) {
+		if (this.behaviours.class.prox) {
+			for (var funct of functions) {
+				funct()
+			}
+		}
+	};
 }
 
 
--- javascripts/entityHelper.js
+++ javascripts/entityHelper.js
@@ -4,18 +4,21 @@ function EntityHelper() {
 
 EntityHelper.prototype.proximity = function(args) {
 	var proximity;
-	xDist = Math.abs(args.subject.coords.x - args.subject.coords.x) -
-					(args.subject.size.x + args.subject.size.x) / 2;
-	yDist = Math.abs(args.subject.coords.y - args.subject.coords.y) -
-					(args.subject.size.y + args.subject.size.y) / 2;
+	xDist = this.getDist(args, 'x')
+	yDist = this.getDist(args, 'y')
 	if (xDist < 0 && yDist < 0) {
 		proximity = 0; // COLLISION!!
 	} else if (xDist > 0 && ydist > 0) {
-		proximity = Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
-	} else if (xDist < 0 && yDist > 0) {
-		proximity = yDist;
-	} else if (yDist < 0 && xDist > 0) {
+		proximity = Math.sqrt(xDist*xDist + yDist*yDist);
+	} else if (xDist > 0 && yDist < 0) {
 		proximity = xDist;
+	} else if (yDist > 0 && xDist < 0) {
+		proximity = yDist;
 	}
 	return proximity;
+}
+
+EntityHelper.prototype.getDist = function(args, axis) {
+	return Math.abs(args.subject.coords.axis - args.object.coords.axis) -
+									(args.subject.size.axis + args.object.size.axis) / 2;
 }
