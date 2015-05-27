1fa9a31b05d864aec37b8599e0e1ef34a9fe6a51
peterjacobson <peterjacobson.nz@gmail.com>
2015-05-27 16:34:38 +1200

    add tests World#constructor

--- tests/world_test.js
+++ tests/world_test.js
@@ -8,5 +8,22 @@ describe("World", function() {
 			var world = new World();
 			expect(world.size).to.exist;
 		})
+
+		it("should call initializeEntities", function() {
+			var world = new World();
+			// var world.renderEntities = sinon.spy();
+			expect(world.initializeEntities.called).to.be.true
+		})
+
+		it("should have an array of entities", function() {
+			var world = new World();
+			expect(world.entities).to.be.a('array')
+		})
+
+		it("should call renderEntities", function() {
+			var world = new World();
+			// var world.renderEntities = sinon.spy();
+			expect(world.renderEntities.called).to.be.true
+		})
 	})
 })
