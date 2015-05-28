c4225bd1cb67942de4ba0d7bf5e899251eaf8817
peterjacobson <peterjacobson.nz@gmail.com>
2015-05-28 21:15:05 +1200

    refactoring tests after eugene pairing

--- tests/worldSpec.js
+++ tests/worldSpec.js
@@ -2,36 +2,32 @@
 var expect = chai.expect;
 
 describe("World", function() {
-	var world;
-	var sandbox = sinon.sandbox.create();
 
 	beforeEach(function() {
-		world = new World();
-		var B = sandbox.stub();
+		this.sandbox = sinon.sandbox.create();
+		this.world = new World();
+		this.B = this.sandbox.stub();
+		console.log(this.B);
 	})
 
 	afterEach(function() {
-		sandbox.restore();
+		this.sandbox.restore();
 	})
 
 	describe("constructor", function() {
-		// it("has a size", function() {
-		// 	expect(world.size).to.exist;
-		// })
 
-		it("calls initializeEntities()", function() {
-			var initializeEntities = sandbox.spy(World.prototype, "initializeEntities");
+		it("initializes 2x 'B' entities & stores them in this.entities", function() {
+			// var fakeEntities = ['entity1', 'entity2']
+			// // var initializeEntities = this.sandbox.stub(World.prototype, "initializeEntities", function() {
+			// 	return fakeEntities
+			// });
+
 			world = new World();
-			expect(initializeEntities.calledOnce).to.be.true;
-			World.prototype.initializeEntities.restore();
+			expect(world.entities).to.equal(fakeEntities);
 		})
 
-		// it("has an array of entities", function() {
-		// 	expect(world.entities).to.be.a('array');
-		// })
-
 		it("calls renderEntities()", function() {
-			var renderEntities = sandbox.spy(World.prototype, 'renderEntities');
+			var renderEntities = this.sandbox.spy(World.prototype, 'renderEntities');
 			world = new World;
 			expect(world.renderEntities.called).to.be.true;
 			World.prototype.renderEntities.restore();
@@ -39,49 +35,42 @@ describe("World", function() {
 	})
 
 	describe("#initializeEntities()", function() {
-		it("returns an array", function() {
-			var response = world.initializeEntities();
-			expect(response).to.be.a('array')
-		})
-
 		it("returned array is not empty", function() {
 			var response = world.initializeEntities();
 			expect(response).to.not.be.empty;
 		})
 
-		it("returned array contains at least one entity") // test for Entity superclass?
+		it("returned array contains at least one entity", function() {
+			var response = world.initializeEntities();
+			expect(response).to.respondTo('klass');
+		}) // test for Entity superclass?
 	})
 
 	describe("#renderEntities()", function() {
 		it("adds all entities to the DOM") // Testable? View-model separation? Samson, help!
 	})
 
-	var response; //help
+
 	describe("#sense()", function() {
-		beforeEach(function() {
+		it("returns an array of all entities except the one with passed id", function() {
 			var B1 = { id: 1 }
 			var B2 = { id: 2 }
-			// console.log(world.entities);
-			world = new World()
-			var worldEntities = sandbox.stub( world, "entities", function() { return [B1, B2]; })
-			console.log(world.sense(1));
-			var response = world.sense(1);
-		})
-		it("returns an array", function() {
-			expect(world.sense(1)).to.be.a('array')
-		})
-
-		it("returns all entities except one", function() {
-			expect(world.sense(1).length).to.equal(world.entities.length-1)
-		})
-
-		it("doesn't return the entity with the id passed", function() {
-			var response = world.sense(0);
-			var entityIDs = [];
-			for (var i = 0; i <  response.length; i++) {
-				 entityIDs.push(response[i].id);
-			};
-			expect(entityIDs).to.not.include(1)
+			var B3 = { id: 3 }
+			// var world = new World()
+			var worldEntities = sandbox.stub( world, "entities", function() { return [B1, B2, B3]; })
+			var response = world.sense(2); // B2
+			// var entityIDs = [];
+			// // var response = world.sense(0);
+			// for (var i = 0; i <  response.length; i++) {
+			// 	 entityIDs.push(response[i].id);
+			// };
+			console.log(sandbox);
+			console.log(response);
+			expect(response).to.be.a('array')
+			console.log(response);
+			expect(response).to.include(B1, B3)
+			console.log(response);
+			expect(response).to.not.include(B2)
 		})
 	})
 })
