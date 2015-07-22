b27d21f2eaa1c183a5c909ede5aecd8c8414ffbb
peterjacobson <peterjacobson.nz@gmail.com>
2015-07-22 16:18:17 +1200

    setup for browserfiying, initial world component

---
+++ index.js
@@ -0,0 +1,5 @@
+var React = require('react')
+
+var World = require('react-components/world')
+
+React.render(<World />, document.body)
--- react-components/world.js
+++ react-components/world.js
@@ -1,2 +1,37 @@
 var React = require('react')
 
+var B = require('b')
+
+module.exports = React.createClass({
+	componentDidMount: function() {
+		
+	},
+	render: function() {
+		var numEntities = 2
+		var entities = []
+		var timeStep = {
+			max: 300,
+			min: 200
+		};
+		for (var i = 1; i <= numEntities; i++) {
+			var timeStep = Math.random() * (timeStep.max - timeStep.min) + timeStep.min
+			entities.push(
+				<B timeStep={timeStep} id={i}/>
+			)
+		}
+
+		return (
+			<div id='world'>
+				{entities}
+			</div>
+		)
+	}
+})
+
+
+
+// World.prototype.sense = function(id) {
+// 	return $.grep(this.entities, function(e) {
+// 		return e.id != id;
+// 	});
+// }
