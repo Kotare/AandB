029abef4d098581822fe7a81b10a9426fc7ee6d5
peterjacobson <peterjacobson.nz@gmail.com>
2015-07-22 16:47:02 +1200

    set b style, create initial state

--- react-components/b.js
+++ react-components/b.js
@@ -1,12 +1,23 @@
 var React = require('react')
+var Victor = require('victor')
 
 module.exports = React.createClass({
-	componentDidMount: function() {
-		
+	getInitialState: function() {
+		return {
+			currentCoords: {
+				x: 0,
+				y: 0
+			},
+			vector: new Victor(0, - (this.pathStep)) //backwards-pointing vector, randomise later
+		};
 	},
 	render: function() {
+		var bStyle = {
+			width: '40px',
+			height: '40px',
+		} 
 		return (
-			<div className='b' id={this.props.id}></div>
+			<div style={bStyle} className='b' id={this.props.id}></div>
 		)
 	}
 })
